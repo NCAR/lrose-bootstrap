d0adaa8b2030b72793d5703f48bd0be7c87d1d8d
mike-dixon <dixon@ucar.edu>
2021-02-03 16:09:32 -0700

    Moving codebase/apps/procmap/src/scripts to codebase/apps/scripts/src

--- scripts/checkout_and_build_cmake.py
+++ scripts/checkout_and_build_cmake.py
@@ -729,13 +729,6 @@ def buildPackage():
             print("running cmd:", cmd, file=logFp)
             shellCmd("rsync -av *pm " + perl5InstallDir)
 
-        # procmap
-
-        procmapScriptsDir = os.path.join(codebaseDir, "apps/procmap/src/scripts")
-        if (os.path.isdir(procmapScriptsDir)):
-            os.chdir(procmapScriptsDir)
-            shellCmd("./install_scripts.lrose " + prefixScriptsDir)
-
         # general
 
         generalScriptsDir = os.path.join(codebaseDir, "apps/scripts/src")
