7b0578e2741ce3060e6f133254d18485670f36e9
mike-dixon <dixon@ucar.edu>
2021-07-02 15:56:54 -0600

    run-solo3-in-docker.py

--- docker/solo3/run-solo3-in-docker.py
+++ docker/solo3/run-solo3-in-docker.py
@@ -2,18 +2,7 @@
 
 #===========================================================================
 #
-# Run CIDD in docker. Wrapper script.
-#
-# This script performs the following steps:
-#
-#   1. clone lrose-core from git
-#   2. clone lrose-netcdf from git
-#   3. setup autoconf Makefile.am files
-#   4. run configure to create makefiles
-#   5. perform the build in 32-bit mode, and install
-#   6. check the build
-#
-# You can optionally specify a release date.
+# Run solo3 in docker. Wrapper script.
 #
 # Use --help to see the command line options.
 #
@@ -36,6 +25,10 @@ def main():
 
     global options
 
+    # get current dir
+
+    thisDir = os.getcwd()
+
     # parse the command line
 
     thisScriptName = os.path.basename(__file__)
@@ -52,25 +45,16 @@ def main():
                       dest='verbose', default=False,
                       action="store_true",
                       help='Set verbose debugging on')
+    parser.add_option('--dir',
+                      dest='dir', default=thisDir,
+                      help='Directory containing the DORADE data')
     parser.add_option('--docker_image',
                       dest='docker_image',
-                      default='nsflrose/lrose-cidd',
-                      help='Set the docker image to run. Should be in DockerHub. Default is "nsflrose/lrose-cidd"')
-    parser.add_option('--params',
-                      dest='params',
-                      default='',
-                      help="Set params file name. For example: 'CIDD.pecan'. In this case the URL would be 'http://front.eol.ucar.edu/displayParams/CIDD.pecan'. i.e. the param file name will be appended to the URL. If the --params option is not used, then the params_url will be used instead.")
-    parser.add_option('--params_url',
-                      dest='params_url',
-                      default='http://front.eol.ucar.edu/displayParams/CIDD.pecan',
-                      help='Set the full URL for CIDD params file. This activates if the --params option is not used.')
-    parser.add_option('--params_local',
-                      dest='params_local',
-                      default='',
-                      help="Set path of local params file. This will be provided to CIDD running in the container.")
+                      default='nsflrose/lrose-solo3',
+                      help='Set the docker image to run. Should be in DockerHub. Default is "nsflrose/lrose-solo3"')
 
     (options, args) = parser.parse_args()
-    
+
     # check OS - is this a mac?
 
     global isOsx
@@ -113,44 +97,19 @@ def main():
     if (options.debug):
         print("Running %s:" % thisScriptName, file=sys.stderr)
         print("  docker image: ", options.docker_image, file=sys.stderr)
-        print("  CIDD params URL: ", options.params_url, file=sys.stderr)
         if (isOsx):
             print("  OS: this is a mac", file=sys.stderr)
         else:
             print("  OS: this is NOT a mac", file=sys.stderr)
         print("  displayStr: ", displayStr, file=sys.stderr)
 
-    # for local params make copy into /tmp
-
-    paramsLocal = False
-    localName = os.path.basename(options.params_local)
-    tmpDir = "/tmp/cidd_params"
-    if (len(options.params_local) > 0):
-        paramsLocal = True
-        try:
-            os.makedirs(tmpDir)
-        except:
-            if (options.verbose):
-                print("Info exists: ", tmpDir, file=sys.stderr)
-        shellCmd("rsync -av " + options.params_local + " " + tmpDir)
-
     # set up call for running docker
     
     cmd = "docker run -v $HOME/.Xauthority:/root/.Xauthority "
-    cmd += "-v /tmp/cidd_images:/root/images "
-    if (paramsLocal):
-        cmd += "-v /tmp/cidd_params:/root/params "
+    cmd += "-v " + options.dir + ":/data "
     cmd += displayStr + " "
     cmd += options.docker_image + " "
-    cmd += "/usr/local/cidd/bin/CIDD -font fixed -p "
-    if (paramsLocal):
-        cmd += "/root/params/" + localName
-    elif (len(options.params) > 0):
-        cmd += "http://front.eol.ucar.edu/displayParams/" + options.params
-    else:
-        cmd += options.params_url
-    if (options.verbose):
-        cmd += " -v 2"
+    cmd += "bash -c \"cd /data; /usr/local/solo3/bin/solo3\" "
 
     # run the command
 
