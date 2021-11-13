fa0049f3047bd360bf539dd60715fdc20dfba55e
mike-dixon <dixon@ucar.edu>
2021-11-13 15:48:11 -0700

    install_linux_packages.py - almalinux

--- scripts/install_linux_packages.py
+++ scripts/install_linux_packages.py
@@ -91,7 +91,7 @@ def main():
         elif (osVersion == 8):
             installPackagesCentos8()
     elif (osType == "almalinux"):
-        if (osVersion.find("8") >= 0):
+        if (osVersion >= 8):
             installPackagesAlmalinux8()
     elif (osType == "fedora"):
          installPackagesFedora()
