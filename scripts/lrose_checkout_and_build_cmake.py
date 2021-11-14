9b29a4d936b347f0c44fb1cb37e020406322c21f
mike-dixon <dixon@ucar.edu>
2021-11-13 18:56:45 -0700

    scripts/lrose_checkout_and_build_cmake.py - adding -DLDLAGS in cmake

--- scripts/lrose_checkout_and_build_cmake.py
+++ scripts/lrose_checkout_and_build_cmake.py
@@ -659,7 +659,10 @@ def buildPackage():
     cmakeBuildDir = os.path.join(codebaseDir, "build")
     os.makedirs(cmakeBuildDir)
     os.chdir(cmakeBuildDir)
-    cmd = cmakeExec + " -DCMAKE_INSTALL_PREFIX=" + prefixDir + " .."
+    cmd = cmakeExec
+    cmd = cmd + " -DCMAKE_INSTALL_PREFIX=" + prefixDir
+    cmd = cmd + " -DLDFLAGS=" + prefixLibDir
+    cmd = cmd + " .."
     shellCmd(cmd)
     
     # build the libraries
