591e3d2964b0bc9dc41289203612bac38d3e83ae
leavesntwigs <bjav7496@gmail.com>
2024-04-08 15:10:17 -0600

    adding files for conda forge packaging

--- conda-forge/conda_packaging/build.sh
+++ conda-forge/conda_packaging/build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash                                                                                                                                                                                                                                   
-echo "PWD = " $(PWD)
-echo "SRC_DIR = " $SRC_DIR
+# echo "SRC_DIR = " $SRC_DIR
 LROSE_CORE=${SRC_DIR}
 echo "LROSE_CORE = " ${LROSE_CORE}
 ## change to source dir                                                                                                                                                                                                                       
 
-# SRC_DIR is like ... /Users/brenda/miniconda3/conda-bld/my-core2_1694565309776/work
-# location of pkgs is /Users/brenda/miniconda3/pkgs/qt-5.15.8-h93fa01e_0.conda
+# SRC_DIR is like ... $HOME/miniconda3/conda-bld/my-core2_1694565309776/work
+# location of pkgs is $HOME/miniconda3/pkgs/qt-5.15.8-h93fa01e_0.conda
 # cd ${SRC_DIR}/code
 
 ## compile                                                                                                                                                                                                                                    
@@ -23,14 +22,18 @@ echo "LROSE_CORE = " ${LROSE_CORE}
 
 # ---------
 
-export HOST_OS=OSX_LROSE
+# export CONDA_MAMBA=miniconda3
+export CONDA_MAMBA=micromamba
+
+# export HOST_OS=OSX_LROSE
+export HOST_OS=LINUX_LROSE
 export LROSE_INSTALL_DIR=/tmp/mylrosetest
 # $HOME/lrose
 export LROSE_CORE_DIR=${LROSE_CORE}
 #  $HOME/git/lrose-core
 
 # Qt path
-export CONDA_PKG_DIR=/Users/brenda/miniconda3/pkgs
+export CONDA_PKG_DIR=$HOME/$CONDA_MAMBA/pkgs
 export QT_PATH=${CONDA_PKG_DIR}/qt-main-5.15.8-hc03889f_16/lib/cmake/Qt5
 # export PKG_CONFIG_PATH="/usr/local/opt/qt/lib/pkgconfig"
 
@@ -45,8 +48,21 @@ export QT_PATH=${CONDA_PKG_DIR}/qt-main-5.15.8-hc03889f_16/lib/cmake/Qt5
 # cd $LROSE_CORE_DIR/codebase/apps; make  install
 
 # this is the cmake way ...
-# CONDA_PKG_DIR=/Users/brenda/miniconda3/pkgs
+# CONDA_PKG_DIR=$HOME/$CONDA_MAMBA/pkgs
 # QT_PATH=${CONDA_PKG_DIR}/qt-main-5.15.8-hc03889f_16/lib/cmake/Qt5
-Qt5_DIR=/Users/brenda/miniconda3/pkgs/qt-main-5.15.8-hc03889f_16/lib/cmake/Qt5
-cd build/cmake; ./createCMakeLists.py --prefix /tmp/mylrosetest/bin 
-cd ${SRC_DIR}/codebase; mkdir build;  cd build;  cmake ..
+Qt5_DIR=$HOME/$CONDA_MAMBA/pkgs/qt-main-5.15.8-hc03889f_16/lib/cmake/Qt5
+cd $SRC_DIR/build/scripts; ./installPackageMakefiles.py --package=lrose-radx
+# trouble with this line ... cd: build/cmake: No such file or directory
+cd $SRC_DIR/build/cmake; ./createCMakeLists.py --silent --prefix /tmp/mylrosetest/bin 
+cd ${SRC_DIR}/codebase; mkdir -p build;  cd build;  cmake ..
+echo "after make ..."
+#ls /tmp/mylrosetest/bin
+
+echo "last line of build.sh"
+
+# try ... and it works
+# cmake -DCMAKE_CXX_COMPILER=$HOME/micromamba/pkgs -DCMAKE_C_COMPILER=$HOME/micromamba/pkgs ..
+
+# /home/gucci/micromamba/pkgs
+# don't use the -DCMAKE stuff, instead add to the requirements: build: in the meta.yaml file.
+
